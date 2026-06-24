f81a67aeff4e83f3afd5218a00d642f4dfe4d793
TEK Student <TEKstudent@vlab.lan>
2026-06-24 08:20:59 -0700

    Remove cloud block

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
+/*
+  cloud {
+    organization = "policy-as-code-training"
+    workspaces {
+      name = "tf-vault-qa-{your-initials}"
     }
   }
+*/
 }
-
 provider "aws" {
   region  = "us-west-1"
 }
@@ -109,8 +111,8 @@ module "elb_http" {
 module "ec2_instances" {
   source = "./modules/aws-instance"
 
-  instance_count     = 2
-  instance_type      = "t2.micro"
+  instance_count     = var.instance_count
+  instance_type      = var.instance_type
   subnet_ids         = module.vpc.private_subnets[*]
   security_group_ids = [module.app_security_group.this_security_group_id]
 
